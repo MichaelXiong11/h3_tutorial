2f8471b3bef6f38af38ba9178e5850ca09f7eaba
michaelxiong11 <michaelxiong11@gmail.com>
2020-11-02 22:07:07 -0600

    commit first passed test

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -2,9 +2,9 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
 
-	public boolean checkISBN(int i) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean checkISBN(int isbn) {
+		
+		return true;
 	}
 
 }
