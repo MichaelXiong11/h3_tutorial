294ea0d2651ddfe7b3ff20af065c7187157e58d0
michaelxiong11 <michaelxiong11@gmail.com>
2020-11-03 01:01:15 -0600

    commit - refactor and test code passes

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -2,32 +2,28 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
 
+	private static final int Long_ISBN_Multiplier = 10;
+	private static final int Short_ISBN_Multiplier = 11;
+	private static final int Short_ISBN_Length = 10;
+	private static final int Long_ISBN_Length = 13;
+
 	public boolean checkISBN(String isbn) {
 		 
-		if(isbn.length() == 13) {
-			int total =0;
-			
-			for(int i = 0; i < 13; i++) {
-				if(i % 2 == 0) {
-					total += Character.getNumericValue(isbn.charAt(i));
-				}else {
-					total += Character.getNumericValue(isbn.charAt(i)) * 3;
-				}
-			}
-			if (total % 10 == 0) {
-				return true;
-			}
-			else {
-				return false;
-			}
+		if(isbn.length() == Long_ISBN_Length) {
+			return isThisValid13NumberISBN(isbn);
 		} 
 		else {
 		
-		if(isbn.length() != 10) throw new NumberFormatException("ISBN number must be 10 digits long");
+		if(isbn.length() != Short_ISBN_Length) throw new NumberFormatException("ISBN number must be 10 digits long");
 		
+		return IsThisAValid10DigitISBN(isbn);
+		}
+	}
+
+	private boolean IsThisAValid10DigitISBN(String isbn) {
 		int total = 0;
 		
-		for(int i = 0; i < 10; i++) 
+		for(int i = 0; i < Short_ISBN_Length; i++) 
 		{
 			if(!Character.isDigit(isbn.charAt(i))) {
 				if(i == 9 && isbn.charAt(i) == 'X'){
@@ -37,16 +33,33 @@ public class ValidateISBN {
 				}
 			}else {
 				
-				total += Character.getNumericValue(isbn.charAt(i)) * (10 -i);
+				total += Character.getNumericValue(isbn.charAt(i)) * (Short_ISBN_Length -i);
 			}
 		}
 		
-		if (total % 11 == 0) {
+		if (total % Short_ISBN_Multiplier == 0) {
 			return true;
 		}
 		else {
 			return false;
 		}
+	}
+
+	private boolean isThisValid13NumberISBN(String isbn) {
+		int total =0;
+		
+		for(int i = 0; i < Long_ISBN_Length; i++) {
+			if(i % 2 == 0) {
+				total += Character.getNumericValue(isbn.charAt(i));
+			}else {
+				total += Character.getNumericValue(isbn.charAt(i)) * 3;
+			}
+		}
+		if (total % Long_ISBN_Multiplier == 0) {
+			return true;
+		}
+		else {
+			return false;
 		}
 	}
 
